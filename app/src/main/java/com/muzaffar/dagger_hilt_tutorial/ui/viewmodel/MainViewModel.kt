5df070b2248441cb7386090fde2fdda5b722a16c
Muzaffar Bagban <muzaffarbagban@yahoo.in>
2021-02-02 16:50:10 +0530

    initial commit

--- app/src/main/java/com/muzaffar/dagger_hilt_tutorial/ui/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/muzaffar/dagger_hilt_tutorial/ui/viewmodel/MainViewModel.kt
@@ -7,13 +7,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.muzaffar.dagger_hilt_tutorial.data.model.User
 import com.muzaffar.dagger_hilt_tutorial.data.repository.MainRepository
-import com.muzaffar.dagger_hilt_tutorial.utils.NetworkHelper
 import com.muzaffar.dagger_hilt_tutorial.utils.Resource
 import kotlinx.coroutines.launch
 
 class MainViewModel @ViewModelInject constructor(
-    private val mainRepository: MainRepository,
-    private val networkHelper: NetworkHelper
+    private val mainRepository: MainRepository
 ) : ViewModel() {
 
     private val _users = MutableLiveData<Resource<List<User>>>()
@@ -27,13 +25,11 @@ class MainViewModel @ViewModelInject constructor(
     private fun fetchUsers() {
         viewModelScope.launch {
             _users.postValue(Resource.loading(null))
-            if (networkHelper.isNetworkConnected()) {
-                mainRepository.getUsers().let {
-                    if (it.isSuccessful) {
-                        _users.postValue(Resource.success(it.body()))
-                    } else _users.postValue(Resource.error(it.errorBody().toString(), null))
-                }
-            } else _users.postValue(Resource.error("No internet connection", null))
+            mainRepository.getUsers().let {
+                if (it.isSuccessful) {
+                    _users.postValue(Resource.success(it.body()))
+                } else _users.postValue(Resource.error(it.errorBody().toString(), null))
+            }
         }
     }
 }
